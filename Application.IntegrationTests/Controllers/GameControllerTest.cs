68053583633d2fcc4c32ca246c3f67c99e0ac534
Luca Vilches Poloni <lucapoloni.main@outlook.com>
2023-02-25 15:46:33 -0300

    tests: integration tests in application layer

---
+++ Application.IntegrationTests/Controllers/GameControllerTest.cs
@@ -0,0 +1,83 @@
+﻿using Application.Aggregates;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+
+namespace Application.IntegrationTests.Controllers
+{
+    public class GameControllerTest : IClassFixture<WebApplicationFactory<Program>>
+    {
+        private readonly HttpClient _httpClient;
+
+        public GameControllerTest(WebApplicationFactory<Program> factory)
+        {
+            _httpClient = factory.CreateClient();
+        }
+
+        [Fact]
+        public async Task PlayPost_WithValidPlayers_ShouldBeStatusCodeOK()
+        {
+            #region Arrange
+            var playerOne = new PlayerAggregate("Luca", "Rock");
+            var playerTwo = new PlayerAggregate("Caio", "Paper");
+            var players = new PlayersAggregate(playerOne, playerTwo);
+            var json = JsonSerializer.Serialize(players);
+            var request = CreateRequest(json);
+            #endregion
+
+            #region Act
+            var response = await _httpClient.SendAsync(request);
+            #endregion
+
+            #region Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK); 
+            #endregion
+        }
+
+        [Fact]
+        public async Task PlayPost_WithInvalidPlayersPropertys_ShouldBeStatusCodeBadRequest()
+        {
+            #region Arrange
+            var invalidPlayerOne = new PlayerAggregate(string.Empty, string.Empty);
+            var invalidPlayerTwo = new PlayerAggregate(string.Empty, string.Empty);
+            var players = new PlayersAggregate(invalidPlayerOne, invalidPlayerTwo);
+            var json = JsonSerializer.Serialize(players);
+            var request = CreateRequest(json);
+            #endregion
+
+            #region Act
+            var response = await _httpClient.SendAsync(request);
+            #endregion
+
+            #region Assert
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            #endregion
+        }
+
+        [Fact]
+        public async Task PlayPost_WithInvalidPlayers_ShouldBeStatusCodeBadRequest()
+        {
+            #region Arrange
+            var request = CreateRequest(string.Empty);
+            #endregion
+
+            #region Act
+            var response = await _httpClient.SendAsync(request);
+            #endregion
+
+            #region Assert
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+            #endregion
+        }
+
+        private static HttpRequestMessage CreateRequest(string json)
+        {
+            return new HttpRequestMessage(HttpMethod.Post, "/api/v1/game/play")
+            {
+                Content = new StringContent(json, Encoding.UTF8, "application/json")
+            };
+        }
+    }
+}
--- Application/Program.cs
+++ Application/Program.cs
@@ -1,32 +1,37 @@
 using Microsoft.OpenApi.Models;
 
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c =>
+namespace Application
 {
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "RockPaperScissors", Version = "v1" });
-    c.EnableAnnotations();
-});
-        
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-var app = builder.Build();
+            // Add services to the container.
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RockPaperScissors v1"));
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "RockPaperScissors", Version = "v1" });
+                c.EnableAnnotations();
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RockPaperScissors v1"));
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
